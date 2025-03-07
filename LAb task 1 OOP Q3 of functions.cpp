b71dff78e375d0b86e944d28af5ccde144448292
ehhsen <ehsansha222@gmail.com>
2025-03-07 16:14:55 +0500

    Update LAb task 1 OOP Q3 of functions.cpp

--- LAb task 1 OOP Q3 of functions.cpp
+++ LAb task 1 OOP Q3 of functions.cpp
@@ -1,5 +1,6 @@
-// Function with default arguments
-/* 3 Lab Task 3: Function Template
+
+/*
+3 Lab Task 3: Function Template
 Objective: Implement a program to demonstrate the use of function templates
 for generic programming.
 Task:
@@ -10,28 +11,38 @@ of arguments:
 – Two integers (e.g., 10 and 20).
 – Two floating-point numbers (e.g., 3.14 and 2.71).
 – Two characters (e.g., ’A’ and ’Z’).
-• Display the values before and after swapping for each function call.
-*/
+• Display the values before and after swapping for each function call.*/
+
 
 #include<iostream>
 #include<iomanip>
 using namespace std;
-
-//void printStudentDetails(char[], int = 0, char = 'N', double = 0.0)
-void printStudentDetails(char name[], int rolNo = 0, char grade = 'N', double marks = 0.0)
+template <class T>
+void swapValues(T &v1, T &v2)
 {
-	cout<<"Name: "<<name;
-	cout<<"\tRoll Number: "<<rolNo;
-	cout<<"\tGrade: "<<grade;
-	cout<<"\tMarks: "<<marks<<endl;
+	cout<<"\nValues before swap: "<<v1<<"\t"<<v2;
+	T swap;
+	swap = v1;
+	 v1 = v2;
+	 v2 = swap;
+	cout<<"\nValues after swap: "<<v1<<"\t"<<v2<<endl;
 }
 int main()
 {
-	char name[50] = "Ahsan ALi";
-	printStudentDetails(name);
-	printStudentDetails(name, 1);
-	printStudentDetails(name, 1, 'A');	
-	printStudentDetails(name, 1, 'A', 87.2);
-	return 0;
-	
+	int a = 10, b = 20;
+	cout<<"\nInteger: \t";
+	swapValues(a, b);
+	cout<<"\nfloat: \t";
+	float v1 = 3.14, v2 = 2.71;
+	swapValues(v1, v2);
+	cout<<"\ncharacter: \t";
+	char c1 = 'A', c2 = 'Z';
+	swapValues(c1, c2);
+	/* display values to check if the values are actually swapped
+	cout<<"\nNew Values"<<endl;
+	cout<<a<<"\t"<<b<<endl;
+	cout<<v1<<"\t"<<v2<<endl;
+	cout<<c1<<"\t"<<c2<<endl;
+	*/
+	return 0;	
 }
